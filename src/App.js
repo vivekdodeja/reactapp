1c73c8b8ef72ef96d1495c450df7b34c42ded8e1
vivekdodeja <vivek.dodeja@gmail.com>
2019-12-08 05:50:59 -0800

    Change message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          wow why is this not working
+          Feature Branch
         </p>
         <a
           className="App-link"
